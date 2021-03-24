9e61df35fe68b1091da45b279973ea25f5f73afb
Jess <jesskaria1@gmail.com>
2021-03-24 20:31:05 +0000

    Ready for deployment

--- client/components/Message/Message.js
+++ client/components/Message/Message.js
@@ -18,6 +18,8 @@ const Message = ({ match, history }) => {
   const id = match.params.id
 
   console.log(chat)
+  console.log(convo)
+  console.log(user)
 
   const [formData, updateFormData] = useState({
     subject: '',
