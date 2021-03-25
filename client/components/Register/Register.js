73a432e9d9ade9ed536874b6283ca59130a5120b
Jess <jesskaria1@gmail.com>
2021-03-25 17:24:42 +0000

    new

--- client/components/Register/Register.js
+++ client/components/Register/Register.js
@@ -18,7 +18,7 @@ const Register = ({ history }) => {
     email: '',
     password: '',
     headline: '',
-    photo: ''
+    photo: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
   })
 
   console.log(formData)
